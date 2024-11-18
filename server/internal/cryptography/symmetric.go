dd0fd2d2565568e35795ecad9c60afb42644d0be
Grzegorz Maniak <git@grzegorz.ie>
2024-11-18 14:05:17 +0000

    Feat: Implemented RSA keypair generation function

--- server/internal/cryptography/symmetric.go
+++ server/internal/cryptography/symmetric.go
@@ -1,1 +1,50 @@
 package cryptography
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+)
+
+type RSAKeyPair struct {
+	PrivateKey []byte
+	PublicKey  []byte
+}
+
+func (rsaKeyPair *RSAKeyPair) EncodePrivateKey() string {
+	return string(rsaKeyPair.PrivateKey)
+}
+
+func (rsaKeyPair *RSAKeyPair) EncodePublicKey() string {
+	return string(rsaKeyPair.PublicKey)
+}
+
+func GenerateRSAKeyPair(length int) (rsaKeyPair *RSAKeyPair, err error) {
+	if length < 2048 {
+		return &RSAKeyPair{}, errors.New("key length must be at least 2048 bits")
+	}
+
+	private, err := rsa.GenerateKey(rand.Reader, length)
+	if err != nil {
+		return &RSAKeyPair{}, err
+	}
+
+	privateBytes := x509.MarshalPKCS1PrivateKey(private)
+	privateBlock := &pem.Block{Type: "RSA PRIVATE KEY", Bytes: privateBytes}
+	privateKey := pem.EncodeToMemory(privateBlock)
+
+	pubBytes, err := x509.MarshalPKIXPublicKey(&private.PublicKey)
+	if err != nil {
+		return &RSAKeyPair{}, err
+	}
+
+	pubBlock := &pem.Block{Type: "PUBLIC KEY", Bytes: pubBytes}
+	publicKey := pem.EncodeToMemory(pubBlock)
+
+	return &RSAKeyPair{
+		PrivateKey: privateKey,
+		PublicKey:  publicKey,
+	}, nil
+}
