1d9f8f1018fce46289544d1107e98f22f5ac80b2
Grzegorz Maniak <git@grzegorz.ie>
2024-11-18 13:48:36 +0000

    Feat: Added DKIM key size to config

--- server/config/structs/auth.go
+++ server/config/structs/auth.go
@@ -4,14 +4,17 @@ import "time"
 
 type RawAuthConfig struct {
 	MaxVerifyTimeWindow int `yaml:"maxVerifyTimeWindow"`
+	DKIMSize            int `yaml:"dkimSize"`
 }
 
 type ParsedAuthConfig struct {
 	MaxVerifyTimeWindow time.Duration
+	DKIMSize            int
 }
 
 func (r RawAuthConfig) Parse() (*ParsedAuthConfig, error) {
 	return &ParsedAuthConfig{
 		MaxVerifyTimeWindow: time.Duration(r.MaxVerifyTimeWindow) * time.Second,
+		DKIMSize:            r.DKIMSize,
 	}, nil
 }
