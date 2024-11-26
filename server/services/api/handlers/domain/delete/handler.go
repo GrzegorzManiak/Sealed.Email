1c8276bd63df7a9f78f1028f227068e307e7b8a2
Grzegorz Maniak <git@grzegorz.ie>
2024-11-26 13:29:44 +0000

    Feat: Added domain remove

--- server/services/api/handlers/domain/delete/handler.go
+++ server/services/api/handlers/domain/delete/handler.go
@@ -3,8 +3,14 @@ package domainDelete
 import (
 	"github.com/GrzegorzManiak/NoiseBackend/internal/helpers"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func handler(data *Input, ctx *gin.Context) (*Output, helpers.AppError) {
-	return nil, nil
+func handler(data *Input, ctx *gin.Context, userID uint, databaseConnection *gorm.DB) (*Output, helpers.AppError) {
+	err := deleteDomain(userID, data.DomainID, databaseConnection)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Output{}, nil
 }
--- server/services/api/handlers/domain/delete/services.go
+++ server/services/api/handlers/domain/delete/services.go
@@ -1,1 +1,32 @@
 package domainDelete
+
+import (
+	"github.com/GrzegorzManiak/NoiseBackend/database/primary/models"
+	"github.com/GrzegorzManiak/NoiseBackend/internal/helpers"
+	"gorm.io/gorm"
+)
+
+func deleteDomain(
+	userID uint,
+	domainID string,
+	databaseConnection *gorm.DB,
+) helpers.AppError {
+	domain := &models.UserDomain{}
+	dbQuery := databaseConnection.Where("user_id = ? AND r_id = ?", userID, domainID).First(domain)
+	if dbQuery.Error != nil {
+		return helpers.GenericError{
+			Message: dbQuery.Error.Error(),
+			ErrCode: 400,
+		}
+	}
+
+	dbDelete := databaseConnection.Delete(domain)
+	if dbDelete.Error != nil {
+		return helpers.GenericError{
+			Message: dbDelete.Error.Error(),
+			ErrCode: 400,
+		}
+	}
+
+	return nil
+}
