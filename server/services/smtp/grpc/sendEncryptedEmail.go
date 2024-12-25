d2d73e5e9842cc1b4e5e501fe25c64d6cec52c87
Grzegorz Maniak <git@grzegorz.ie>
2024-12-25 19:49:27 +0000

    Feat: Implemented service functions

--- server/services/smtp/grpc/sendEncryptedEmail.go
+++ server/services/smtp/grpc/sendEncryptedEmail.go
@@ -1,1 +1,10 @@
 package grpc
+
+import (
+	"context"
+	"github.com/GrzegorzManiak/NoiseBackend/proto/smtp"
+)
+
+func (s *Server) SendEncryptedEmail(ctx context.Context, encryptedEmail *smtp.EncryptedEmail) (*smtp.SendEmailResponse, error) {
+	return &smtp.SendEmailResponse{Success: true}, nil
+}
--- server/services/smtp/grpc/sendPublicEmail.go
+++ server/services/smtp/grpc/sendPublicEmail.go
@@ -1,1 +1,10 @@
 package grpc
+
+import (
+	"context"
+	"github.com/GrzegorzManiak/NoiseBackend/proto/smtp"
+)
+
+func (s *Server) SendPublicEmail(ctx context.Context, publicEmail *smtp.PublicEmail) (*smtp.SendEmailResponse, error) {
+	return &smtp.SendEmailResponse{Success: true}, nil
+}
