9204a0300cdf96292ceeb3cdaa98ec1b0179c500
Grzegorz Maniak <git@grzegorz.ie>
2024-12-25 20:05:14 +0000

    Feat: Added inbound and outbound smtp queues

--- server/services/smtp/grpc/grpcService.go
+++ server/services/smtp/grpc/grpcService.go
@@ -1,11 +1,16 @@
 package grpc
 
 import (
+	"github.com/GrzegorzManiak/NoiseBackend/internal/queue"
 	"github.com/GrzegorzManiak/NoiseBackend/proto/smtp"
 	"gorm.io/gorm"
 )
 
 type Server struct {
 	smtp.UnimplementedSmtpServiceServer
-	MainDatabaseConnection *gorm.DB
+	MainDatabaseConnection  *gorm.DB
+	QueueDatabaseConnection *gorm.DB
+
+	InboundQueue  *queue.Queue
+	OutboundQueue *queue.Queue
 }
--- server/services/smtp/smtp.go
+++ server/services/smtp/smtp.go
@@ -4,23 +4,40 @@ import (
 	"context"
 	"github.com/GrzegorzManiak/NoiseBackend/config"
 	PrimaryDatabase "github.com/GrzegorzManiak/NoiseBackend/database/primary"
+	SmtpDatabase "github.com/GrzegorzManiak/NoiseBackend/database/smtp"
+	"github.com/GrzegorzManiak/NoiseBackend/internal/queue"
 	ServiceProvider "github.com/GrzegorzManiak/NoiseBackend/internal/service"
 	"github.com/GrzegorzManiak/NoiseBackend/proto/smtp"
 	"github.com/GrzegorzManiak/NoiseBackend/services/smtp/grpc"
-	"github.com/GrzegorzManiak/NoiseBackend/services/smtp/services"
 	"go.uber.org/zap"
 )
 
 func Start() {
 	zap.L().Info("Starting smtp service")
 
+	queueDatabaseConnection := SmtpDatabase.InitiateConnection()
 	primaryDatabaseConnection := PrimaryDatabase.InitiateConnection()
 
-	services.ExampleServer()
+	inboundQueue := queue.NewQueue(
+		queueDatabaseConnection,
+		config.Smtp.InboundQueue.Name,
+		config.Smtp.InboundQueue.BatchTimeout,
+		config.Smtp.InboundQueue.MaxConcurrent,
+	)
+
+	outboundQueue := queue.NewQueue(
+		queueDatabaseConnection,
+		config.Smtp.OutboundQueue.Name,
+		config.Smtp.OutboundQueue.BatchTimeout,
+		config.Smtp.OutboundQueue.MaxConcurrent,
+	)
 
 	listener, grpcServer, ServiceID := ServiceProvider.CreateGRPCService(config.Certificates.SMTP)
 	smtp.RegisterSmtpServiceServer(grpcServer, &grpc.Server{
-		MainDatabaseConnection: primaryDatabaseConnection,
+		MainDatabaseConnection:  primaryDatabaseConnection,
+		QueueDatabaseConnection: queueDatabaseConnection,
+		InboundQueue:            inboundQueue,
+		OutboundQueue:           outboundQueue,
 	})
 
 	serviceAnnouncement := ServiceProvider.Announcement{
