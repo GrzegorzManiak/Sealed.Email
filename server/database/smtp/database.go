f55c95b17c3958dc8159a7e6ee525c8b31e8b0d3
Grzegorz Maniak <git@grzegorz.ie>
2024-12-25 20:03:26 +0000

    Feat: Added models for smtp queue entry

---
+++ server/database/smtp/database.go
@@ -0,0 +1,26 @@
+package smtp
+
+import (
+	"github.com/GrzegorzManiak/NoiseBackend/database/smtp/models"
+	"go.uber.org/zap"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"moul.io/zapgorm2"
+)
+
+func InitiateConnection() *gorm.DB {
+	logger := zapgorm2.New(zap.L())
+	logger.SetAsDefault()
+	driver := sqlite.Open("./dev/smtp.db")
+	databaseConnection, err := gorm.Open(driver, &gorm.Config{Logger: logger})
+	if err != nil {
+		zap.L().Panic("failed to connect to database", zap.Error(err))
+	}
+
+	AutoMigrateTables(databaseConnection)
+	return databaseConnection
+}
+
+func AutoMigrateTables(databaseConnection *gorm.DB) {
+	databaseConnection.AutoMigrate(&models.VerificationQueue{})
+}
