87181e0fad12cd60101f7172e9d555fd2da3b5e4
Grzegorz Maniak <git@grzegorz.ie>
2024-12-26 17:09:31 +0000

    Feat: Added Inbound SMTP server settings

--- server/config/structs/smtp.go
+++ server/config/structs/smtp.go
@@ -9,7 +9,23 @@ type SmtpQueueConfig struct {
 	BatchTimeout  int    `yaml:"batchTimeout"`
 }
 
+type SmtpPorts struct {
+	Tls      int `yaml:"tls"`
+	Plain    int `yaml:"plain"`
+	StartTls int `yaml:"startTls"`
+}
+
 type SmtpConfig struct {
-	OutboundQueue SmtpQueueConfig `yaml:"outboundQueue"`
-	InboundQueue  SmtpQueueConfig `yaml:"inboundQueue"`
+	OutboundQueue SmtpQueueConfig     `yaml:"outboundQueue"`
+	InboundQueue  SmtpQueueConfig     `yaml:"inboundQueue"`
+	Certificates  ServiceCertificates `yaml:"certificates"`
+	Ports         SmtpPorts           `yaml:"ports"`
+
+	Address           string `yaml:"address"`
+	Domain            string `yaml:"domain"`
+	WriteTimeout      int    `yaml:"writeTimeout"`
+	ReadTimeout       int    `yaml:"readTimeout"`
+	MaxMessageBytes   int64  `yaml:"maxMessageBytes"`
+	MaxRecipients     int    `yaml:"maxRecipients"`
+	AllowInsecureAuth bool   `yaml:"allowInsecureAuth"`
 }
