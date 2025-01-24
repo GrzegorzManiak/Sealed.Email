82c7d3c2ae280b2f336a385b3861dde46927354f
Grzegorz Maniak <git@grzegorz.ie>
2025-01-24 13:53:16 +0000

    Refactor: Got rid of useless files

--- server/services/smtp/grpc/sendEmail.go
+++ server/services/smtp/grpc/sendEmail.go
@@ -5,6 +5,6 @@ import (
 	"github.com/GrzegorzManiak/NoiseBackend/proto/smtp"
 )
 
-func (s *Server) SendEncryptedEmail(ctx context.Context, encryptedEmail *smtp.EncryptedEmail) (*smtp.SendEmailResponse, error) {
+func (s *Server) SendEmail(ctx context.Context, email *smtp.Email) (*smtp.SendEmailResponse, error) {
 	return &smtp.SendEmailResponse{Success: true}, nil
 }
