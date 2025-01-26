82b7a95dd09225bac8f604896511ee157c419225
Grzegorz Maniak <git@grzegorz.ie>
2025-01-26 15:50:26 +0000

    Feat: Added outbound email store

--- server/services/smtp/grpc/sendEmail.go
+++ server/services/smtp/grpc/sendEmail.go
@@ -3,8 +3,14 @@ package grpc
 import (
 	"context"
 	"github.com/GrzegorzManiak/NoiseBackend/proto/smtp"
+	"github.com/GrzegorzManiak/NoiseBackend/services/smtp/client"
 )
 
 func (s *Server) SendEmail(ctx context.Context, email *smtp.Email) (*smtp.SendEmailResponse, error) {
+	_, err := client.QueueEmail(email, s.QueueDatabaseConnection, s.OutboundQueue)
+	if err != nil {
+		return &smtp.SendEmailResponse{Success: false}, err
+	}
+
 	return &smtp.SendEmailResponse{Success: true}, nil
 }
