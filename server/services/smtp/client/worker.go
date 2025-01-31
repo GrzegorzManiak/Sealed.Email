62f86e7ea3d48323e248cb240c5e1da915869fdf
Grzegorz Maniak <git@grzegorz.ie>
2025-01-31 12:47:52 +0000

    Debug: Changed log level

--- server/services/smtp/client/worker.go
+++ server/services/smtp/client/worker.go
@@ -91,7 +91,7 @@ func Worker(certs *tls.Config, entry *queue.Entry, queueDatabaseConnection *gorm
 	}
 
 	if err := services.VerifyDns(fromDomain, email.Challenge); err != nil {
-		zap.L().Error("Failed to verify dns", zap.Error(err))
+		zap.L().Debug("Failed to verify dns", zap.Error(err))
 		return 2
 	}
 
