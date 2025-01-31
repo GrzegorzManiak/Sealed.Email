b4cf969a7858daade7bea02bd4af55382a02ce54
Grzegorz Maniak <git@grzegorz.ie>
2025-01-31 13:04:20 +0000

    Feat: Added send encrypted route

--- server/services/api/routes/email.go
+++ server/services/api/routes/email.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"fmt"
+	sendEncrypted "github.com/GrzegorzManiak/NoiseBackend/services/api/handlers/email/send/encrypted"
 	sendPlain "github.com/GrzegorzManiak/NoiseBackend/services/api/handlers/email/send/plain"
 	"github.com/GrzegorzManiak/NoiseBackend/services/api/services"
 	"github.com/gin-gonic/gin"
@@ -11,5 +12,8 @@ func EmailRoutes(router *gin.Engine, baseRoute *services.BaseRoute) {
 	fmt.Println("Registering routes for: email")
 	router.POST("/api/email/send/plain", func(ctx *gin.Context) {
 		services.ExecuteRoute[sendPlain.Input, sendPlain.Output](ctx, baseRoute, sendPlain.SessionFilter, sendPlain.Handler)
+	})
+	router.POST("/api/email/send/encrypted", func(ctx *gin.Context) {
+		services.ExecuteRoute[sendEncrypted.Input, sendEncrypted.Output](ctx, baseRoute, sendEncrypted.SessionFilter, sendEncrypted.Handler)
 	})
 }
